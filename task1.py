5404ee3fe29de89e018be542735cd17c703495be
Kanishk1176 <77190197+Kanishk1176@users.noreply.github.com>
2025-12-11 01:02:25 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,11 @@
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+sum_ = n1 + n2
+difference = n1 - n2
+product = n1 * n2
+quotient = n1 / n2
+print("Addition: ",sum_)
+print("Subtraction: ",difference)
+print("Multiplication: ",product,)
+print("Division: ",round(quotient,2))
+
